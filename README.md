dade4927402ec092de977430a2900a0a8c54b0e2
adbun61 <blunndell.ad@gmail.com>
2025-08-05 14:46:22 +0100

    Improve todo app with date, desciprtion, and DB function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # To_do_list
+
+# First attempt at using github 
+
+# Project is creating a to do list in the command line with python and storing the list into a basic db for recovering if you close the application down
+
+# Update added in function for adding a description as well as add the date it was added onto the to-do list, and creating a db using sqlite3 for retaining the to-do list
--- To_do_list.py
+++ To_do_list.py
@@ -1,46 +1,99 @@
 # Simmple to do list whihch continue to edit and update 
 
+import sqlite3
+from datetime import datetime
+
+
+# Connect to the SQLite database
+conn = sqlite3.connect('todo_list.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        description TEXT,
+        date TEXT,
+        status TEXT DEFAULT 'pending'
+    )
+''')
+
+# function for adding tasks
+def add_task(task, description):
+    date_added = datetime.now().strftime('%Y-%m-%d')
+    cursor.execute("INSERT INTO tasks (title, date, description) VALUES (?,?,?)", (task, date_added, description))
+    conn.commit()
+
+
+#function for showing all the task 
+def show_task():
+    cursor.execute("SELECT * FROM tasks")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"{row[0]}. {row[1]}. {row[2]}. {row[3]}. {row[4]}")
+
+
+# function for marking the task as done
+def complete_task(task_id):
+   cursor.execute(' UPDATE tasks SET status = 'complete' WHERE id = ?', (task_id,))
+   conn.commit() 
+
+
+def delete_task(task_id):
+    cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+    conn.commit()
+
+# Maiin loop
 def main():
-    tasks = []
 
     while True:
         print("\n==== To-Do List ====")
         print("1. Add Task")
         print("2. Show Tasks")
         print("3. Mark Task as done")
-        print("4. Exit")
+        print("4. Delete a Task")
+        print("5. Exit")
 
         choice = input(" Enter your choice: ")
 
         if choice == '1':
-            print()
-            n_tasks = int(input("How many task do you want to add: "))
+            task = input("Please enter the task you would like to add: ")
+            while True:
+                add_description = input("Would you like to add a description? ")
+                if add_description == "yes":
+                    description = input("Please enter a description: ")
+                    break
+                elif add_description == "no":
+                    description = "N/A"
+                    break
+                else:
+                    print(f"incorrect choice please try again ")
 
-            for i in range (n_tasks):
-                task = input("Enter the task: ")
-                tasks.append({"task": task, "done": False})
-                print('Task added')
+            add_task(task, description)
+
     
         elif choice == '2':
-            print("\nTask:")
-            for index, task in enumerate(tasks):
-                status = "Done" if task["done"] else "Not Done"
-                print(f"{index +1}. {task['task']} - {status}")
+            show_task()
 
         elif choice == '3':
-            task_index = int(input("Enter the task number to mark as done: ")) -1
-            if  0 <= task_index < len(tasks):
-                tasks[task_index]["done"] = True
-                print("Task marked as done!")
-            else:
-                print("Invalid task number")
-    
+            try:
+                task_id = int(input("Enter task ID: "))
+                complete_task(task_id)
+            except ValueError:
+                    print("Invalid task ID. Must be a number.")
+
+
         elif choice == '4':
-            print("Exiting the To-Do List.")
+            task_id = input("What task ID do you want to delete? ")
+            delete_task(task_id)
+    
+        elif choice == '5':
+            print("\nExiting the To-Do List. ")
+            conn.close()
             break
 
         else:
-            print(f"Invalid choice. Please try agin")
+            print(f"\nInvalid choice. Please try agin")
     
 if __name__ == "__main__":
     main()
